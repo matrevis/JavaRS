ec3d43bd4db2e8f0c854a1f20831032118a6361f
Trevisan Marco <3vis93@gmail.com>
2019-11-14 18:24:22 +0100

    add jpa, need to fix postgresql in heroku

---
+++ services/src/main/java/it/trevis/beans/DataExample.java
@@ -0,0 +1,28 @@
+package it.trevis.beans;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DataExampleJPA")
+public class DataExample implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_data_gen")
+	@SequenceGenerator(sequenceName = "seq_data", name = "seq_data_gen")
+	private BigInteger id;
+	
+	@Column(name = "something")
+	private String something;
+
+}
---
+++ services/src/main/java/it/trevis/dao/SomethingDao.java
@@ -0,0 +1,62 @@
+package it.trevis.dao;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import it.secservizi.w4.kpi.beans.DataFromHomeBanking;
+
+@Stateless
+@LocalBean
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+public class SomethingDao implements Serializable{
+	
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
+
+	private static final long serialVersionUID = 1L;
+
+	protected static final String PERSISTENCE_UNIT_NAME = "db_something";
+	
+	@PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
+	protected EntityManager entityManager;
+	
+	public SomethingDao() {
+		logger.trace("SomethingDao: creato oggetto SomethingDao..");
+	}
+
+}
+
+//	public BigInteger getId(DataFromHomeBanking entity) {
+//		return entity.getId();
+//	}
+	
+	// save
+//	public DataFromHomeBanking save(DataFromHomeBanking entity) {
+//		try {
+//			entityManager.persist(entity);
+//		}
+//		catch(Exception e) {
+//			log.error("Errore nel tentativo di inserire i dati in DB..", e);
+//		}
+//		return entity;
+//	}
+	
+	// read
+//	public DataFromHomeBanking read(DataFromHomeBanking entity) {
+//		try {	
+//			entityManager.find(entity.getClass(), entity.getId());
+//		}
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//		return entity;
+//	}
