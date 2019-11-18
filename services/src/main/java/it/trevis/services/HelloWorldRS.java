3074b07cf58e753b7c9f30ac85c8e8369a3d34b4
Marco <3vis93@gmail.com>
2019-11-19 00:42:47 +0100

    add logback xml

--- services/src/main/java/it/trevis/services/HelloWorldRS.java
+++ services/src/main/java/it/trevis/services/HelloWorldRS.java
@@ -6,17 +6,17 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+//import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.PathSegment;
+//import javax.ws.rs.core.PathSegment;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +39,7 @@ public class HelloWorldRS {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAll() {
 		logger.debug("Chiamata a getAll()..");
+		System.out.println(MethodHandles.lookup().lookupClass().getSimpleName());
 		List<DataExample> result = dao.read();
 		return Response.ok(jsonb.toJson(result), MediaType.APPLICATION_JSON).build();
 	}
